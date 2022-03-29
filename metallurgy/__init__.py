2f8ec6050cf93791e7161bb97807dbf8e53040fe
Robert Forrest <robertforrest@live.com>
2022-03-29 13:07:14 +0100

    Set __all__ properly

--- metallurgy/__init__.py
+++ metallurgy/__init__.py
@@ -3,3 +3,9 @@ from .alloy import Alloy
 from elementy.periodictable import PeriodicTable
 
 periodic_table = PeriodicTable()
+
+__all__ = [
+    "linear_mixture",
+    "Alloy",
+    "periodic_table"
+]
