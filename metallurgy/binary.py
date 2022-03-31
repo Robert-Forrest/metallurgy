fb5bba1f6ba384116eb586f8e666ff33eb54e627
Robert Forrest <robertforrest@live.com>
2022-03-31 09:53:49 +0100

    Bug fix: Inclide zero in range of percentages

--- metallurgy/binary.py
+++ metallurgy/binary.py
@@ -6,7 +6,7 @@ def generate_alloys(elements, step=0.5):
     x = 100
     alloys = []
     percentages = []
-    while x > 0:
+    while x >= 0:
         alloys.append(Alloy(
             elements[0] + str(x) + elements[1] + str(100 - x)))
         percentages.append(x)
