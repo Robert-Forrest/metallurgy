81cc11e726a01986ce3dbe35a88a4764cb212785
Robert Forrest <robertforrest@live.com>
2022-03-31 13:33:54 +0100

    Reversed order of ternary composition generator

--- metallurgy/ternary.py
+++ metallurgy/ternary.py
@@ -4,29 +4,35 @@ from .alloy import Alloy
 def generate_alloys(elements, step=1, minPercent=0, maxPercent=100, quaternary=None):
 
     compositions = []
-    allPercentages = []
-    percentages = [minPercent - step, minPercent - step, minPercent - step]
-    while percentages[0] <= maxPercent:
-        percentages[0] += step
-        percentages[1] = minPercent - step
-        percentages[2] = minPercent - step
-        while percentages[1] <= maxPercent:
-            percentages[1] += step
-            percentages[2] = minPercent - step
-            while percentages[2] <= maxPercent:
-                percentages[2] += step
-                if(sum(percentages) == 100):
-                    allPercentages.append(percentages[:])
+    percentages = []
+
+    tmp_percentages = [maxPercent+step]*3
+    while tmp_percentages[0] >= minPercent+step:
+        tmp_percentages[0] -= step
+        tmp_percentages[1] = maxPercent+step
+        tmp_percentages[2] = maxPercent+step
+
+        while tmp_percentages[1] >= minPercent+step:
+            tmp_percentages[1] -= step
+            tmp_percentages[2] = maxPercent + step
+
+            while tmp_percentages[2] >= minPercent+step:
+                tmp_percentages[2] -= step
+
+                if(sum(tmp_percentages) == 100):
+
                     composition_str = ""
                     for i in range(len(elements)):
-                        if(percentages[i] > 0):
+                        if(tmp_percentages[i] > 0):
                             composition_str += elements[i] + \
-                                str(percentages[i])
+                                str(tmp_percentages[i])
 
                     if quaternary is not None:
                         composition_str = "(" + composition_str + ")" + str(
                             100 - quaternary[1]) + quaternary[0] + str(quaternary[1])
 
-                    compositions.append(Alloy(composition_str))
+                    alloy = Alloy(composition_str)
+                    compositions.append(alloy)
+                    percentages.append(list(alloy.composition.values()))
 
-    return compositions, allPercentages
+    return compositions, percentages
