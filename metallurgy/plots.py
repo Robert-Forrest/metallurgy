5542cb62ade1da469fcf73aaddcf343afe95432c
Robert Forrest <robertforrest@live.com>
2022-03-31 13:34:31 +0100

    Added ternary heatmap plots

--- metallurgy/plots.py
+++ metallurgy/plots.py
@@ -1,6 +1,9 @@
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.collections as mcoll
+import matplotlib as mpl
+import ternary as ternary_plt
 
 
 def binary(alloys, data, xlabel=None, ylabel=None, labels=None, scatter_data=None, use_colorline=False, save_path=None):
@@ -67,6 +70,93 @@ def binary(alloys, data, xlabel=None, ylabel=None, labels=None, scatter_data=Non
     plt.cla()
     plt.clf()
     plt.close()
+
+
+def ternary_heatmap(alloys, data, step=0.01, scatter_data=None,
+                    save_path=None, quaternary=None, label=None, title=None,
+                    vmin=None, vmax=None, ax=None, showColorbar=True):
+
+    scale = 1 / step
+    multiple = 0.1 / step
+    fontsize = 10
+    tick_fontsize = 6
+    tick_offset = 0.018
+    gridline_width = 0.3
+    gridline_style = '--'
+    gridline_color = 'white'
+
+    elements = []
+    for alloy in alloys:
+        for element in alloy.elements:
+            if element not in elements:
+                elements.append(element)
+
+    if not isinstance(data, dict):
+        data = list(data)
+
+        heatmap_data = dict()
+
+        for i in range(len(data)):
+            percent_A = 0
+            percent_B = 0
+
+            if(elements[0] in alloys[i].composition):
+                percent_A = round(alloys[i].composition[elements[0]] / step, 2)
+            if(elements[1] in alloys[i].composition):
+                percent_B = round(alloys[i].composition[elements[1]] / step, 2)
+
+            heatmap_data[(percent_A, percent_B)] = data[i]
+
+        data = heatmap_data
+
+    if ax is None:
+        figure, tax = ternary_plt.figure(scale=scale)
+    else:
+        figure, tax = ternary_plt.figure(scale=scale, ax=ax)
+
+    tax.get_axes().axis('off')
+
+    tax.gridlines(color=gridline_color, multiple=multiple,
+                  linewidth=gridline_width, ls=gridline_style)
+
+    tax.set_axis_limits(
+        {'b': [0, 100], 'l': [0, 100], 'r': [0, 100]})
+    tax.get_ticks_from_axis_limits(multiple=multiple)
+    tax.set_custom_ticks(
+        fontsize=tick_fontsize, offset=tick_offset, multiple=multiple)
+
+    tax.left_axis_label(elements[2], fontsize=fontsize, offset=0.12)
+    tax.right_axis_label(elements[1], fontsize=fontsize, offset=0.12)
+    tax.bottom_axis_label(elements[0], fontsize=fontsize, offset=0.12)
+    tax.clear_matplotlib_ticks()
+
+    tax.set_title(title)
+
+    jet_cmap = mpl.cm.get_cmap('jet')
+    tax.heatmap(heatmap_data, cmap=jet_cmap, vmax=vmax,
+                vmin=vmin, cbarlabel=label, colorbar=showColorbar)
+    # ternary_scatter(realData, tax)
+
+    if scatter_data is not None:
+        for scatter_datum in scatter_data:
+            tax.scatter(scatter_datum['data'],
+                        marker=scatter_datum['marker'],
+                        label=scatter_datum['label'],
+                        edgecolors='k', zorder=20)
+
+        tax.legend(loc="upper right", handletextpad=0.1, frameon=False)
+
+    tax.get_axes().set_aspect(1)
+    tax._redraw_labels()
+
+    if ax is None:
+        if save_path is None:
+            tax.show()
+        else:
+            tax.savefig(save_path + '.png')
+
+        tax.close()
+        figure.clf()
 
 
 def colorline(x, y, ax, z=None, cmap='jet', norm=plt.Normalize(0.0, 100.0), linewidth=2, alpha=1.0):
