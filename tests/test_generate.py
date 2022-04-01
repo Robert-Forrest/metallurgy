79c06602e91cef05e736cc1c1e398dc5716a206e
Robert Forrest <robertforrest@live.com>
2022-04-01 22:40:28 +0100

    More detailed required_elements check

--- tests/test_generate.py
+++ tests/test_generate.py
@@ -42,3 +42,7 @@ def test_requirements():
     for element in required_elements:
         if required_elements[element]['min'] > 0:
             assert element in random_alloy.composition
+
+        if element in random_alloy.composition:
+            assert random_alloy.composition[element] >= required_elements[element]['min']
+            assert random_alloy.composition[element] <= required_elements[element]['max']
