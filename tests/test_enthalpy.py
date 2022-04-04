c493f755e0a8090f527a65c9a7c0b92592aa8eb0
Robert Forrest <robertforrest@live.com>
2022-04-04 15:30:44 +0100

    Updated enthalpy test

--- tests/test_enthalpy.py
+++ tests/test_enthalpy.py
@@ -3,5 +3,5 @@ import metallurgy as mg
 
 
 def test_mixing_enthalpy():
-    assert mg.enthalpy.calculate_mixing_enthalpy("Cu50Zr50") == pytest.approx(-16.988864976589436)
-    assert mg.enthalpy.calculate_mixing_enthalpy("Cu") == 0
+    assert mg.enthalpy.mixing_enthalpy("Cu50Zr50") == pytest.approx(-16.988864976589436)
+    assert mg.enthalpy.mixing_enthalpy("Cu") == 0
