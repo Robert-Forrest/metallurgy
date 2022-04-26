b7b10de9415289cdb6910d1cff3b37d9328ec643
Robert Forrest <robertforrest@live.com>
2022-04-26 09:45:04 +0100

    Added helpers to analysis funcs to generate alloy systems

--- metallurgy/analyse.py
+++ metallurgy/analyse.py
@@ -9,21 +9,25 @@ def find_max(elements, feature_name):
     if isinstance(elements, str):
         elements = re.findall('[A-Z][^A-Z]*', elements)
 
-    alloys, percentages, values = generate.system(elements, feature_name=feature_name)
+    system = generate.system(elements, feature_name=feature_name)
+    if system is not None:
 
-    max_index = np.argmax(values)
+        alloys, percentages, values = system
 
-    return alloys[max_index], values[max_index]
+        max_index = np.argmax(values)
+
+        return alloys[max_index], values[max_index]
 
 
 def find_min(elements, feature_name):
     if isinstance(elements, str):
         elements = re.findall('[A-Z][^A-Z]*', elements)
 
-    alloys, percentages, values = generate.system(elements, feature_name=feature_name)
+    system = generate.system(elements, feature_name=feature_name)
+    if system is not None:
 
-    print(values)
+        alloys, percentages, values = system
 
-    min_index = np.argmin(values)
+        min_index = np.argmin(values)
 
-    return alloys[min_index], values[min_index]
+        return alloys[min_index], values[min_index]
