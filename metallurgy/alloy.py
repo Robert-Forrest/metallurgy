442333f286c453d51fe69bcddd747008c5b7e923
Robert Forrest <robertforrest@live.com>
2022-10-13 18:41:14 +0100

    Bug fix: Throw error if no composition is set

--- metallurgy/alloy.py
+++ metallurgy/alloy.py
@@ -56,6 +56,9 @@ class Alloy:
     ):
 
         self.composition = parse_composition(composition)
+        if self.composition is None:
+            raise Exception("Invalid composition:", composition)
+
         self.constraints = None
 
         if rescale:
