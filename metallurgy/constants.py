ddb8dac93900fd6e549bd864a607911f5aa622a3
Robert Forrest <robertforrest@live.com>
2022-10-17 12:18:51 +0100

    Constants docs

--- metallurgy/constants.py
+++ metallurgy/constants.py
@@ -1,3 +1,11 @@
+"""``metallurgy.constants``
+=============================
+
+Module providing some physical constants used in calculation functions.
+
+"""
+
+
 plankConstant = 6.63e-34
 avogadroNumber = 6.022e23
 idealGasConstant = 8.31
