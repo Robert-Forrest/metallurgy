604c0c792a556bca370712aa0f0d0da4198ae7ce
Robert Forrest <robertforrest@live.com>
2022-11-16 21:53:01 +0000

    Added periodic table plotting function

--- metallurgy/plots.py
+++ metallurgy/plots.py
@@ -1,13 +1,43 @@
+from typing import List, Optional
+
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.collections as mcoll
 import matplotlib as mpl
 import ternary as ternary_plt
+from bokeh.sampledata.periodic_table import elements
+from bokeh.models import (
+    ColumnDataSource,
+    LinearColorMapper,
+    LogColorMapper,
+    ColorBar,
+    BasicTicker,
+)
+from bokeh.plotting import figure
+from bokeh.io import export_png
+from bokeh.io import show as show_
+from bokeh.transform import dodge
+
 
 import metallurgy as mg
 
 
-def plot(alloys, data, label=None):
+def plot(alloys: list, data: list, label: str = None):
+    """An automatic interface to metallurgy's alloy system plotting
+    functions.
+
+    Parameters
+    ----------
+
+    alloys
+        List of alloys being plotted.
+    data
+        List of data belonging to alloys to plot.
+    label
+        String for labelling the plotted data.
+
+    """
+
     if isinstance(alloys[0], mg.Alloy):
         num_elements = len(mg.analyse.find_unique_elements(alloys))
     elif isinstance(alloys[0], list) and isinstance(alloys[0][0], mg.Alloy):
@@ -412,9 +442,219 @@ def make_segments(x, y):
     return segments
 
 
-def pretty_percentage(string):
+def pretty_percentage(string: str) -> str:
+    """Trim trailing decimals from percentage string"""
     while string[-1] == "0":
         string = string[:-1]
     if string[-1] == ".":
         string = string[:-1]
     return string
+
+
+def periodic_table(
+    data,
+    show: bool = True,
+    output_filename: Optional[str] = None,
+    width: int = 1050,
+    cmap: str = "viridis",
+    alpha: float = 0.65,
+    extended: bool = True,
+    periods_remove: List[int] = None,
+    groups_remove: List[int] = None,
+    log_scale: bool = False,
+    cbar_height: float = None,
+    cbar_standoff: int = 12,
+    cbar_fontsize: int = 14,
+    blank_color: str = "#c4c4c4",
+    under_value: float = None,
+    under_color: str = "#140F0E",
+    over_value: float = None,
+    over_color: str = "#140F0E",
+    special_elements: List[str] = None,
+    special_color: str = "#6F3023",
+):
+    """
+    Plot a heatmap over the periodic table of elements.
+    Adapted from work of Andrew Rosen at
+    https://github.com/arosen93/ptable_trends
+
+    Parameters
+    ----------
+    filename : str
+        Path to the .csv file containing the data to be plotted.
+    show : str
+        If True, the plot will be shown.
+    output_filename : str
+        If not None, the plot will be saved to the specified (.html) file.
+    width : float
+        Width of the plot.
+    cmap : str
+        plasma, inferno, viridis, magma, cividis, turbo
+    alpha : float
+        Alpha value (transparency).
+    extended : bool
+        If True, the lanthanoids and actinoids will be shown.
+    periods_remove : List[int]
+        Period numbers to be removed from the plot.
+    groups_remove : List[int]
+        Group numbers to be removed from the plot.
+    log_scale : bool
+        If True, the colorbar will be logarithmic.
+    cbar_height : int
+        Height of the colorbar.
+    cbar_standoff : int
+        Distance between the colorbar and the plot.
+    cbar_fontsize : int
+        Fontsize of the colorbar label.
+    blank_color : str
+        Hexadecimal color of the elements without data.
+    under_value : float
+        Values <= under_value will be colored with under_color.
+    under_color : str
+        Hexadecimal color to be used for the lower bound color.
+    over_value : float
+        Values >= over_value will be colored with over_color.
+    under_color : str
+        Hexadecial color to be used for the upper bound color.
+    special_elements: List[str]
+        List of elements to be colored with special_color.
+    special_color: str
+        Hexadecimal color to be used for the special elements.
+    """
+
+    values = [data[e] for e in data]
+
+    if cmap == "viridis":
+        cmap = mpl.cm.viridis
+        bokeh_palette = "Viridis256"
+
+    period_label = ["1", "2", "3", "4", "5", "6", "7"]
+    group_range = [str(x) for x in range(1, 19)]
+
+    period_label.append("blank")
+    period_label.append("La")
+    period_label.append("Ac")
+
+    count = 0
+    for i in range(56, 70):
+        elements.period[i] = "La"
+        elements.group[i] = str(count + 4)
+        count += 1
+
+    count = 0
+    for i in range(88, 102):
+        elements.period[i] = "Ac"
+        elements.group[i] = str(count + 4)
+        count += 1
+
+    if log_scale:
+        for datum in data:
+            if datum < 0:
+                raise ValueError(
+                    f"Entry for element {datum} is negative but log-scale is selected"
+                )
+        color_mapper = LogColorMapper(
+            palette=bokeh_palette, low=min(values), high=max(values)
+        )
+        norm = mpl.colors.LogNorm(vmin=min(values), vmax=max(values))
+    else:
+        color_mapper = LinearColorMapper(
+            palette=bokeh_palette, low=min(values), high=max(values)
+        )
+        norm = mpl.colors.Normalize(vmin=min(values), vmax=max(values))
+    color_scale = mpl.cm.ScalarMappable(norm=norm, cmap=cmap).to_rgba(
+        values, alpha=None
+    )
+
+    # Set blank color
+    color_list = [blank_color] * len(elements)
+    # Compare elements in dataset with elements in periodic table
+    for i, data_element in enumerate(data):
+        element_entry = elements.symbol[
+            elements.symbol.str.lower() == data_element.lower()
+        ]
+        if not element_entry.empty:
+            element_index = element_entry.index[0]
+
+        if under_value is not None and data[i] <= under_value:
+            color_list[element_index] = under_color
+        elif over_value is not None and data[i] >= over_value:
+            color_list[element_index] = over_color
+        else:
+            color_list[element_index] = mpl.colors.to_hex(color_scale[i])
+
+    if special_elements:
+        for k, v in elements["symbol"].iteritems():
+            if v in special_elements:
+                color_list[k] = special_color
+
+    # Define figure properties for visualizing data
+    source = ColumnDataSource(
+        data=dict(
+            group=[str(x) for x in elements["group"]],
+            period=[str(y) for y in elements["period"]],
+            sym=elements["symbol"],
+            atomic_number=elements["atomic number"],
+            type_color=color_list,
+        )
+    )
+
+    # Plot the periodic table
+    p = figure(
+        x_range=group_range, y_range=list(reversed(period_label)), tools="save"
+    )
+    p.plot_width = width
+    p.outline_line_color = None
+    p.background_fill_color = None
+    p.border_fill_color = None
+    p.toolbar_location = "above"
+    p.rect(
+        "group",
+        "period",
+        0.9,
+        0.9,
+        source=source,
+        alpha=alpha,
+        color="type_color",
+    )
+    p.axis.visible = False
+    text_props = {
+        "source": source,
+        "angle": 0,
+        "color": "black",
+        "text_align": "left",
+        "text_baseline": "middle",
+    }
+    x = dodge("group", -0.4, range=p.x_range)
+    y = dodge("period", 0.3, range=p.y_range)
+    p.text(
+        x=x,
+        y="period",
+        text="sym",
+        text_font_style="bold",
+        text_font_size="16pt",
+        **text_props,
+    )
+    p.text(x=x, y=y, text="atomic_number", text_font_size="11pt", **text_props)
+
+    color_bar = ColorBar(
+        color_mapper=color_mapper,
+        ticker=BasicTicker(desired_num_ticks=10),
+        border_line_color=None,
+        label_standoff=cbar_standoff,
+        location=(0, 0),
+        orientation="vertical",
+        scale_alpha=alpha,
+        major_label_text_font_size=f"{cbar_fontsize}pt",
+    )
+
+    if cbar_height is not None:
+        color_bar.height = cbar_height
+
+    p.add_layout(color_bar, "right")
+    p.grid.grid_line_color = None
+
+    if output_filename:
+        export_png(p, filename=output_filename)
+    else:
+        show_(p)
